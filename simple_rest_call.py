039e59ffb5dbf855051fa390dd5ebd7b081f63f3
David Conmy <david.conmy@sap.com>
2018-07-14 21:57:37 +0100

    Added REST call for when sensor detected in_range

---
+++ simple_rest_call.py
@@ -0,0 +1,9 @@
+import requests
+
+server_url = 'http://localhost:9090'
+
+r = requests.post(server_url, data={'distance': (1.5 * 100)});
+if r.status_code == 200:
+	print('POST to the server Okay!')
+else:
+	print('Failed to POST to server')
--- ultrasonic_distance_detector.py
+++ ultrasonic_distance_detector.py
@@ -25,8 +25,18 @@
 
 
 from gpiozero import DistanceSensor
+import requests
 
-sensor = DistanceSensor(echo=18, trigger=17, threshold_distance=1.5)
+server_url = 'http://localhost:9090'
+threshold_distance = 1.5
+
+sensor = DistanceSensor(echo=18, trigger=17, threshold_distance=threshold_distance)
+
 while True:
 	sensor.wait_for_in_range()
+	r = requests.post(server_url, data={'distance': (sensor.distance * 100)});
+	if r.status_code == 200:
+		print('Pushed distance of ', sensor.distance * 100, ' to the server')
+	else:
+		print('Failed to push distance to server')
 	print('Detected Distance: ', sensor.distance * 100)
